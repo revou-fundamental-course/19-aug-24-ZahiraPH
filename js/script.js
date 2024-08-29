28b5916a8be30790a8c8aa65fd22cded1e1c2c98
ZahiraPH <zahirapriyanhusna@gmail.com>
2024-08-29 20:40:37 +0700

    second commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,39 @@
 // ini js
+function validateForm() {
+    const name = document.forms['message-form'] ['name-input'].value
+    const angka = 11
+
+    if (name == "") {
+        document.getElementById("error-name").innerHTML = "Tidak boleh kosong!"
+
+        return false
+    } 
+
+    setname(name)
+    
+    return false;
+}
+
+
+function setname(name) {
+    document.getElementById("display-name").innerHTML = name;
+    document.getElementById('error-name').innerHTML = ""
+    
+}
 
 function formValidation() {
     //Ini cara ambil DOM
-    let name = document.getElementById('name').value;
+    let name = document.getElementById('your-name').value;
+    let gender = document.getElementById('gender').value;
+    let dob = document.getElementById('dob').value;
+    let message = document.getElementById('message').value;
     // Ini kondisi pemisahnya
     // == artinya menyamakan. misal: apakah name ini sama dengan ''
     // (!=) tidak sama dengan
-    if (name == ''){
+    if (name === '' || gender === '' || dob === '' || message === ''){
         alert('Harap isi form');
         // kondisi jika formnya kosong
-        document.getElementById('result').innerHTML = '-';
+        //document.getElementById('result').innerHTML = '-';
     }
     else {
         // alert('Form berhasil di submit');
@@ -18,12 +42,16 @@ function formValidation() {
         // name bisa di rubah rubah
         // ini namanya DOM
         // kondisi jika formnya di isi
-        document.getElementById('result').innerHTML = name;
+        //let resultText = `Nama: ${name}<br>Jenis Kelamin: ${gender}<br>Tanggal Lahir: ${dob}<br>Pesan: ${message}`;
+        document.getElementById('result-name').innerText = name;
+        document.getElementById('result-gender').innerText = gender;
+        document.getElementById('result-dob').innerText = dob;
+        document.getElementById('result-message').innerText = message;
     }
 }
 
 let indexSlide = 1;
-showBanner(1):
+showBanner(1);
 
 function nextSlide(n) {
     showBanner (indexSlide += n);
